07c9104716bd8d4234564a04d5abe354d6ac3369
AutomationWithPiyush <automation.with.piyush@gmail.com>
2025-08-05 16:28:29 +0530

    DropDown done

--- A11-Basic-sselenium/src/test/java/dropDown/SingleSelectDropDown.java
+++ A11-Basic-sselenium/src/test/java/dropDown/SingleSelectDropDown.java
@@ -1,6 +1,7 @@
 package dropDown;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,20 +10,47 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class SingleSelectDropDown {
-	 public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-		
-		driver.get("C:/Users/User/git/A11-Basic-selenium/A11-Basic-sselenium/src/test/java/dropDown/ssdd.html");
-		
-		Thread.sleep(1000);
 
+		driver.get("C:/Users/User/git/A11-Basic-selenium/A11-Basic-sselenium/src/test/java/dropDown/ssdd.html");
+
+//		create the webelement object for dropdown component
 		WebElement carDD = driver.findElement(By.id("cars"));
+//		create the object for select class
 		Select selCar = new Select(carDD);
+
+//		check whether the given dd is multiselect or not
+		boolean status = selCar.isMultiple();
+		if (status) {
+			System.out.println("Given dd is multi select");
+		} else {
+			System.out.println("Given dd is single select");
+		}
+
 		selCar.selectByIndex(3);
-		
+		Thread.sleep(1000);
+		selCar.selectByIndex(0);
+
+		List<WebElement> options = selCar.getAllSelectedOptions();
+		for (WebElement i : options) {
+			System.out.println(i.getText());
+			Thread.sleep(100);
+		}
+
+		WebElement firstSelected = selCar.getFirstSelectedOption();
+		System.out.println(firstSelected.getText());
+
+		Thread.sleep(2000);
+		selCar.deselectByIndex(3);
+		Thread.sleep(1000);
+		selCar.deselectByIndex(0);
+
+		selCar.deselectAll();
+
 		Thread.sleep(5000);
-		driver.close();
+		driver.quit();
 	}
 }
