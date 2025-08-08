e551e572050593c0036cda979aae7fad79922ef4
AutomationWithPiyush <automation.with.piyush@gmail.com>
2025-08-08 11:22:19 +0530

    keyboard done

--- A11-Basic-sselenium/src/test/java/working_with_actions/Dustbin.java
+++ A11-Basic-sselenium/src/test/java/working_with_actions/Dustbin.java
@@ -9,40 +9,39 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class Dustbin {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-
-		driver.get("https://marcojakob.github.io/dart-dnd/basic/");
-
 		Actions act = new Actions(driver);
 
-		WebElement kachra1 = driver.findElement(By.className("document"));
-		WebElement peti1 = driver.findElement(By.className("trash"));
-		act.dragAndDrop(kachra1, peti1).build().perform();
-
-		WebElement kachra2 = driver.findElement(By.className("document"));
-		WebElement peti2 = driver.findElement(By.className("full"));
-		act.dragAndDrop(kachra2, peti2).build().perform();
-
-		WebElement kachra3 = driver.findElement(By.className("document"));
-		WebElement peti3 = driver.findElement(By.className("full"));
-		act.dragAndDrop(kachra3, peti3).build().perform();
-
-		WebElement kachra4 = driver.findElement(By.className("document"));
-		WebElement peti4 = driver.findElement(By.className("full"));
-		act.dragAndDrop(kachra4, peti4).build().perform();
-		
+//		driver.get("https://marcojakob.github.io/dart-dnd/basic/");
+//
+//
+//		WebElement kachra1 = driver.findElement(By.className("document"));
+//		WebElement peti1 = driver.findElement(By.className("trash"));
+//		act.dragAndDrop(kachra1, peti1).build().perform();
+//
+//		WebElement kachra2 = driver.findElement(By.className("document"));
+//		WebElement peti2 = driver.findElement(By.className("full"));
+//		act.dragAndDrop(kachra2, peti2).build().perform();
+//
+//		WebElement kachra3 = driver.findElement(By.className("document"));
+//		WebElement peti3 = driver.findElement(By.className("full"));
+//		act.dragAndDrop(kachra3, peti3).build().perform();
+//
+//		WebElement kachra4 = driver.findElement(By.className("document"));
+//		WebElement peti4 = driver.findElement(By.className("full"));
+//		act.dragAndDrop(kachra4, peti4).build().perform();
+//		
 		
 		
 		driver.get("https://jqueryui.com/droppable/");
 		
 		driver.switchTo().frame(0);
 		
+		Thread.sleep(1500);
 		WebElement drag = driver.findElement(By.id("draggable"));
-		WebElement drop = driver.findElement(By.id("droppable"));
-		
-		act.dragAndDrop(drag, drop).build().perform();
+		act.dragAndDropBy(drag, 150,50).build().perform();
 	}
 }
---
+++ A11-Basic-sselenium/src/test/java/working_with_actions/FacebookKeyBoard.java
@@ -0,0 +1,43 @@
+package working_with_actions;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookKeyBoard {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+		driver.get("https://www.facebook.com/");
+
+		Actions act = new Actions(driver);
+
+//		act.sendKeys("admin" + Keys.TAB + "1234567890" + Keys.ENTER).build().perform();
+
+//		enter admin	
+		act.sendKeys("admin").build().perform();
+
+//		select all + copy
+
+//		go to pwd => press tab & release it
+		act.keyDown(Keys.TAB).build().perform();
+		act.keyUp(Keys.TAB).build().perform();
+
+//		paste
+
+//		press enter
+		act.keyDown(Keys.ENTER).build().perform();
+		act.keyUp(Keys.ENTER).build().perform();
+
+		Thread.sleep(5000);
+		driver.quit();
+	}
+}
